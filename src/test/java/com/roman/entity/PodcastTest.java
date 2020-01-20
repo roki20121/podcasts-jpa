9c9660bd4a497c45da4dbd97eac43c13590477ed
Roman_Kishko <roman_kishko@epam.com>
2020-01-20 15:34:54 +0200

    Common test tools moved to separate class

--- src/test/java/com/roman/entity/PodcastTest.java
+++ src/test/java/com/roman/entity/PodcastTest.java
@@ -9,20 +9,13 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PodcastTest {
-
-    private static String user = "postgres";
-    private static String password = "postgres";
 
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("com.roman.podcasts");
     private static EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -30,13 +23,13 @@ class PodcastTest {
     @BeforeAll
     static void createNewDB() throws SQLException, IOException {
         Path scriptPath = Paths.get("scripts/create_all_tables.sql");
-        executeScript(scriptPath);
+        TestUtils.executeScript(scriptPath);
     }
 
     @AfterAll
     static void dropDB() throws SQLException, IOException {
         Path scriptPath = Paths.get("scripts/drop_all_tables.sql");
-        executeScript(scriptPath);
+        TestUtils.executeScript(scriptPath);
     }
 
     @Test
@@ -88,12 +81,4 @@ class PodcastTest {
                 () -> new Podcast("uuid", null));
     }
 
-    private static void executeScript(Path scriptPath) throws IOException, SQLException {
-        String dbUrl = "jdbc:postgresql://localhost:5432/podcasts_db";
-        try (Connection connection = DriverManager.getConnection(dbUrl, user, password);
-             Statement statement = connection.createStatement()) {
-            String script = String.join("\n", Files.readAllLines(scriptPath));
-            statement.execute(script);
-        }
-    }
 }
---
+++ src/test/java/com/roman/entity/TestUtils.java
@@ -0,0 +1,24 @@
+package com.roman.entity;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestUtils {
+
+    private static String user = "postgres";
+    private static String password = "postgres";
+
+    static void executeScript(Path scriptPath) throws IOException, SQLException {
+        String dbUrl = "jdbc:postgresql://localhost:5432/podcasts_db";
+        try (Connection connection = DriverManager.getConnection(dbUrl, user, password);
+             Statement statement = connection.createStatement()) {
+            String script = String.join("\n", Files.readAllLines(scriptPath));
+            statement.execute(script);
+        }
+    }
+}
