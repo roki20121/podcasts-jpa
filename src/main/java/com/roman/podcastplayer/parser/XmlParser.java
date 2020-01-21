05e9ce01cb5c06cc6946b6a8cd319d17a5c2f022
Roman_Kishko <roman_kishko@epam.com>
2020-01-21 14:05:19 +0200

    Channel parser implemented

---
+++ src/main/java/com/roman/podcastplayer/parser/XmlParser.java
@@ -0,0 +1,160 @@
+package com.roman.podcastplayer.parser;
+
+
+import com.roman.podcastplayer.entity.Channel;
+import com.roman.podcastplayer.entity.Podcast;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class XmlParser {
+
+    private static final SimpleDateFormat simpleDateFormat =
+            new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+
+    private InputStream inputStream;
+    private String lastStoredGuid;
+
+    private List<Podcast> podcasts = new ArrayList<>();
+
+    private boolean hasNewPodcasts;
+    private Channel channel;
+
+    public XmlParser(InputStream inputStream) {
+        this(inputStream, null);
+    }
+
+    public XmlParser(InputStream inputStream, String lastStoredGuid) {
+        this.inputStream = inputStream;
+        this.lastStoredGuid = lastStoredGuid;
+    }
+
+    public void parse() throws IOException {
+        try {
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(inputStream, null);
+            Channel channel = null;
+            Podcast podcast = null;
+            boolean inPodcast = false;
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String name;
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        name = parser.getName();
+                        switch (name) {
+                            case "channel":
+                                channel = new Channel();
+                                inPodcast = false;
+                                break;
+                            case "item":
+                                podcast = new Podcast();
+                                inPodcast = true;
+                                break;
+                            case "title":
+                                String title = parser.nextText().trim();
+                                if (inPodcast) {
+                                    podcast.setTitle(title);
+                                } else {
+                                    channel.setTitle(title);
+                                }
+                                break;
+                            case "description":
+                                String desc = parser.nextText().trim();
+                                if (inPodcast) {
+                                    podcast.setDescription(desc);
+                                } else {
+                                    channel.setDescription(desc);
+                                }
+                                break;
+                            case "enclosure":
+                                if (inPodcast) {
+                                    String url = parser.getAttributeValue(0);
+                                    String type = parser.getAttributeValue(null, "type");
+                                    if (type != null) {
+                                        podcast.setAudioUrl(url);
+                                    }
+                                }
+                                break;
+                            case "pubDate":
+                                String pubDate = parser.nextText();
+                                Instant instant = getPublishInstant(pubDate);
+                                if (inPodcast) {
+                                    podcast.setPublished(instant);
+                                } else {
+                                    channel.setLastPublished(instant);
+                                }
+                                break;
+                            case "guid":
+                                if (inPodcast) {
+                                    String guid = parser.nextText().trim();
+                                    podcast.setUuid(guid);
+                                }
+                                break;
+                        }
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        name = parser.getName();
+                        if (name.equals("channel")) {
+                            this.channel = channel;
+                        }
+                        if (name.equals("item")) {
+                            if (podcast.getUuid().equals(lastStoredGuid)) {
+                                this.channel = channel;
+                                return;
+                            }
+                            hasNewPodcasts = true;
+                            podcasts.add(podcast);
+                        }
+                }
+                eventType = parser.next();
+            }
+
+            this.channel = channel;
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean hasNewPodcasts() {
+        return hasNewPodcasts;
+    }
+
+    public Channel getChannel() {
+        if (!hasNewPodcasts)
+            throw new IllegalStateException("Nothing new!");
+        channel.setPodcasts(podcasts);
+        if (channel.getLastPublished() == null && podcasts.size() > 0) {
+            channel.setLastPublished(podcasts.get(0).getPublished());
+        }
+
+        return channel;
+    }
+
+
+    private Instant getPublishInstant(String pubDate) {
+        try {
+            Date parse = simpleDateFormat.parse(pubDate);
+            return Instant.ofEpochMilli(parse.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
---
+++ src/test/java/com/roman/podcastplayer/parser/XmlParserTest.java
@@ -0,0 +1,123 @@
+package com.roman.podcastplayer.parser;
+
+import com.roman.podcastplayer.entity.Channel;
+import com.roman.podcastplayer.entity.Podcast;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XmlParserTest {
+
+    @ParameterizedTest
+    @MethodSource("listFullFileNames")
+    void parse_newChannel_parsedAllContent(String fileName) throws IOException {
+        File podcastFile = getResourceFile(fileName);
+        FileInputStream inputStream = new FileInputStream(podcastFile);
+        XmlParser xmlParser = new XmlParser(inputStream);
+
+        xmlParser.parse();
+
+        assertTrue(xmlParser.hasNewPodcasts());
+
+        Channel channel = xmlParser.getChannel();
+
+        assertNotNull(channel);
+        assertNotNull(channel.getPodcasts());
+        assertNotNull(channel.getLastPublished());
+        assertNotNull(channel.getTitle());
+        assertNotNull(channel.getDescription());
+
+        for (Podcast podcast : channel.getPodcasts()) {
+            assertNotNull(podcast.getUuid());
+            assertNotNull(podcast.getPublished());
+            assertNotNull(podcast.getTitle());
+            assertNotNull(podcast.getAudioUrl());
+            assertNotNull(podcast.getDescription());
+
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getKnownChannelsAndExpectedValues")
+    void parse_knownChannel_newContent(String fileName, String lastSavedUuid, int newPodcastsCount)
+            throws IOException {
+        File channelFile = getResourceFile(fileName);
+        FileInputStream inputStream = new FileInputStream(channelFile);
+        XmlParser xmlParser = new XmlParser(inputStream, lastSavedUuid);
+
+        xmlParser.parse();
+
+        assertTrue(xmlParser.hasNewPodcasts());
+
+        Channel channel = xmlParser.getChannel();
+
+        assertNotNull(channel);
+        assertEquals(newPodcastsCount, channel.getPodcasts().size());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getKnownChannel")
+    void parse_knownChannel_noNewContent(String fileName, String lastSavedUuid) throws IOException {
+        File channelFile = getResourceFile(fileName);
+        FileInputStream inputStream = new FileInputStream(channelFile);
+        XmlParser xmlParser = new XmlParser(inputStream, lastSavedUuid);
+
+        xmlParser.parse();
+
+        assertFalse(xmlParser.hasNewPodcasts());
+    }
+
+    @Test
+    void getChannel_nothingNew_throwsException() {
+        XmlParser xmlParser = new XmlParser(new ByteArrayInputStream(new byte[0]));
+
+        assertThrows(IllegalStateException.class,
+                xmlParser::getChannel);
+    }
+
+    private static Stream<Arguments> getKnownChannel() {
+        return Stream.of(
+                Arguments.of("channels/cbc_full.xml", "undertheinfluence-8bebd2a3-6654-4e4c-9f60-cfa3d0c516ad"),
+                Arguments.of("channels/the_moth_full.xml", "prx_24_9b728a50-99d0-466f-8c07-6621bea4a4fa"),
+                Arguments.of("channels/art_of_charm_full.xml", "gid://art19-episode-locator/V0/StxxKuNYrav27a_PSdhKhF7asz3A4zsgvNdgy4VXJWE")
+        );
+    }
+
+    private static Stream<Arguments> getKnownChannelsAndExpectedValues() {
+        return Stream.of(
+                Arguments.of("channels/cbc_full.xml", "undertheinfluence-0d4bf888-d020-46eb-bc3d-be04233182dc", 1),
+                Arguments.of("channels/cbc_full.xml", "undertheinfluence-b567546b-df58-4e98-b49e-fcc6ca0d9deb", 2),
+                Arguments.of("channels/the_moth_full.xml", "prx_24_a67c9505-49be-43c8-aa0e-860e3277ee98", 1),
+                Arguments.of("channels/the_moth_full.xml", "prx_24_d7d54a46-bfbc-48ef-9bfd-45c9ef976be1", 2),
+                Arguments.of("channels/art_of_charm_full.xml", "gid://art19-episode-locator/V0/fBMA9nY-3IOT5AUT1jgQ7dolw6jhlnDOjvIxYDVPhtU", 1),
+                Arguments.of("channels/art_of_charm_full.xml", "gid://art19-episode-locator/V0/s50ISCTCkW6cqBQoZLHu8BQ-RfNK_h5kpzECHmKRFDs", 2)
+        );
+    }
+
+    private static List<String> listFullFileNames() {
+        String[] names = {
+                "channels/cbc_full.xml",
+                "channels/the_moth_full.xml",
+                "channels/art_of_charm_full.xml"
+        };
+
+        return Arrays.asList(names);
+    }
+
+    private static File getResourceFile(String pathInResources) {
+        String path = XmlParserTest.class.getClassLoader().getResource(pathInResources).getPath();
+        return new File(path);
+    }
+
+}
