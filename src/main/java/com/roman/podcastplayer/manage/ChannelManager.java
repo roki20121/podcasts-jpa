cbe9c25297b86d143cd9bc0499435c661faacb9b
Roman_Kishko <roman_kishko@epam.com>
2020-01-24 12:29:50 +0200

    ChannelManager refactored

--- src/main/java/com/roman/podcastplayer/manage/ChannelManager.java
+++ src/main/java/com/roman/podcastplayer/manage/ChannelManager.java
@@ -2,7 +2,6 @@ package com.roman.podcastplayer.manage;
 
 import com.roman.podcastplayer.entity.Channel;
 import com.roman.podcastplayer.entity.Podcast;
-import com.roman.podcastplayer.parser.ChannelParser;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,8 +9,6 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import java.io.IOException;
-import java.util.Objects;
 
 public class ChannelManager {
 
@@ -47,35 +44,6 @@ public class ChannelManager {
 
         manager.close();
         return result;
-    }
-
-
-    /**
-     * Merges channel retrieved from parser with oldChannel
-     *
-     * @param parser     unused object
-     * @param oldChannel old channel
-     * @return if parser.hasNewPodcasts() returns same object asparser.getChannel(), copies old channel
-     * id, starred and appends old podcasts in chronological order. Else returns oldChannel
-     */
-    public static Channel getUpdatedChannel(ChannelParser parser, Channel oldChannel) throws IOException {
-
-        Objects.requireNonNull(oldChannel);
-        parser.parse();
-
-        if (parser.hasNewPodcasts()) {
-            Channel newChannel = parser.getChannel();
-
-            if (oldChannel.getId() != null) {
-                newChannel.setId(oldChannel.getId());
-            }
-
-            newChannel.setStarred(oldChannel.isStarred());
-            newChannel.getPodcasts().addAll(oldChannel.getPodcasts());
-            return newChannel;
-        } else {
-            return oldChannel;
-        }
     }
 
 }
--- src/test/java/com/roman/podcastplayer/manage/ChannelManagerTest.java
+++ src/test/java/com/roman/podcastplayer/manage/ChannelManagerTest.java
@@ -3,17 +3,11 @@ package com.roman.podcastplayer.manage;
 import com.roman.podcastplayer.entity.Channel;
 import com.roman.podcastplayer.entity.ChannelUtils;
 import com.roman.podcastplayer.entity.Podcast;
-import com.roman.podcastplayer.parser.ChannelParser;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.List;
-import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class ChannelManagerTest {
@@ -43,82 +37,67 @@ class ChannelManagerTest {
     }
 
 
-    @Test
-    void getUpdatedChannel_nothingNew_returnsOldChannel() throws IOException {
+//    @Test
+//    void getUpdatedChannel_nothingNew_returnsOldChannel() throws IOException {
+//
+//        ChannelParser parser = mock(ChannelParser.class);
+//        when(parser.hasNewPodcasts()).thenReturn(false);
+//
+//        Channel channel = new Channel();
+//
+//        Channel updatedChannel = ChannelManager.getUpdatedChannel(parser, channel);
+//
+//        assertSame(channel, updatedChannel);
+//    }
+//
+//    @Test
+//    void getUpdatedChannel_oldChannelNull_throwsException() {
+//        ChannelParser parser = mock(ChannelParser.class);
+//
+//        assertThrows(NullPointerException.class,
+//                () -> ChannelManager.getUpdatedChannel(parser, null));
+//    }
 
-        ChannelParser parser = mock(ChannelParser.class);
-        when(parser.hasNewPodcasts()).thenReturn(false);
-
-        Channel channel = new Channel();
-
-        Channel updatedChannel = ChannelManager.getUpdatedChannel(parser, channel);
-
-        assertSame(channel, updatedChannel);
-    }
-
-    @Test
-    void getUpdatedChannel_oldChannelNull_throwsException() {
-        ChannelParser parser = mock(ChannelParser.class);
-
-        assertThrows(NullPointerException.class,
-                () -> ChannelManager.getUpdatedChannel(parser, null));
-    }
-
-    @Test
-    void getUpdatedChannel_newPodcasts_returnMerged() throws IOException {
-        int id = 9;
-        int newPodcastsCount = 2;
-        int oldPodcastsCount = 10;
-        String url = generateRandomString();
-        ChannelParser parser = mock(ChannelParser.class);
-        when(parser.hasNewPodcasts()).thenReturn(true);
-
-
-        Channel updatesOnly = generateChannel(newPodcastsCount, url);
-        Channel oldChannel = generateChannel(oldPodcastsCount, url);
-        oldChannel.setId(id);
-
-        when(parser.getChannel()).thenReturn(updatesOnly);
-
-        Channel updatedChannel = ChannelManager.getUpdatedChannel(parser, oldChannel);
-
-
-        assertSame(updatesOnly, updatedChannel);
-        assertEquals(id, updatedChannel.getId());
-        assertEquals(url, updatedChannel.getUrl());
-        assertEquals(updatesOnly.getDescription(), updatedChannel.getDescription());
-        assertEquals(updatesOnly.getLastPublished(), updatedChannel.getLastPublished());
-        assertEquals(updatedChannel.isStarred(), oldChannel.isStarred());
-
-        List<Podcast> updatedPodcasts = updatedChannel.getPodcasts();
-        List<Podcast> updatesOnlyPodcasts = updatesOnly.getPodcasts();
-        List<Podcast> oldPodcasts = oldChannel.getPodcasts();
-
-        assertEquals(updatedPodcasts.size(), newPodcastsCount + oldPodcastsCount);
-        assertEquals(updatesOnlyPodcasts.get(0), updatedPodcasts.get(0));                                    // first new podcast
-        assertEquals(updatesOnlyPodcasts.get(newPodcastsCount - 1), updatedPodcasts.get(newPodcastsCount - 1));  // last new podcast
-        assertEquals(oldPodcasts.get(0), updatedPodcasts.get(newPodcastsCount));                             // first old
-        assertEquals(oldPodcasts.get(oldPodcastsCount - 1),
-                updatedPodcasts.get(newPodcastsCount + oldPodcastsCount - 1));                                  // last old
+//    @Test
+//    void getUpdatedChannel_newPodcasts_returnMerged() throws IOException {
+//        int id = 9;
+//        int newPodcastsCount = 2;
+//        int oldPodcastsCount = 10;
+//        String url = generateRandomString();
+//        ChannelParser parser = mock(ChannelParser.class);
+//        when(parser.hasNewPodcasts()).thenReturn(true);
+//
+//
+//        Channel updatesOnly = generateChannel(newPodcastsCount, url);
+//        Channel oldChannel = generateChannel(oldPodcastsCount, url);
+//        oldChannel.setId(id);
+//
+//        when(parser.getChannel()).thenReturn(updatesOnly);
+//
+//        Channel updatedChannel = ChannelManager.getUpdatedChannel(parser, oldChannel);
+//
+//
+//        assertSame(updatesOnly, updatedChannel);
+//        assertEquals(id, updatedChannel.getId());
+//        assertEquals(url, updatedChannel.getUrl());
+//        assertEquals(updatesOnly.getDescription(), updatedChannel.getDescription());
+//        assertEquals(updatesOnly.getLastPublished(), updatedChannel.getLastPublished());
+//        assertEquals(updatedChannel.isStarred(), oldChannel.isStarred());
+//
+//        List<Podcast> updatedPodcasts = updatedChannel.getPodcasts();
+//        List<Podcast> updatesOnlyPodcasts = updatesOnly.getPodcasts();
+//        List<Podcast> oldPodcasts = oldChannel.getPodcasts();
+//
+//        assertEquals(updatedPodcasts.size(), newPodcastsCount + oldPodcastsCount);
+//        assertEquals(updatesOnlyPodcasts.get(0), updatedPodcasts.get(0));                                    // first new podcast
+//        assertEquals(updatesOnlyPodcasts.get(newPodcastsCount - 1), updatedPodcasts.get(newPodcastsCount - 1));  // last new podcast
+//        assertEquals(oldPodcasts.get(0), updatedPodcasts.get(newPodcastsCount));                             // first old
+//        assertEquals(oldPodcasts.get(oldPodcastsCount - 1),
+//                updatedPodcasts.get(newPodcastsCount + oldPodcastsCount - 1));                                  // last old
+//
+//
+//    }
+//    }
 
 
-    }
-
-    private static Channel generateChannel(int podcastsCount, String url) {
-        Channel channel = new Channel(url, generateRandomString());
-
-        channel.setDescription(channel.getDescription());
-        channel.setLastPublished(Instant.now());
-        channel.setStarred(true);
-        for (int i = 0; i < podcastsCount; i++) {
-            Podcast podcast = new Podcast(generateRandomString(), generateRandomString());
-            channel.getPodcasts().add(podcast);
-        }
-
-        return channel;
-    }
-
-    private static String generateRandomString() {
-        return UUID.randomUUID().toString();
-    }
 }
