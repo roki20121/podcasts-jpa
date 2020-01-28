77c20c1994ea0ffd06a2167b183c776a294336ee
Roman_Kishko <roman_kishko@epam.com>
2020-01-28 17:40:02 +0200

    PodcastManager added. Return single podcast by id, star podcast

--- src/main/java/com/roman/podcastplayer/rest/endpoint/PodcastEndpoint.java
+++ src/main/java/com/roman/podcastplayer/rest/endpoint/PodcastEndpoint.java
@@ -1,14 +1,14 @@
 package com.roman.podcastplayer.rest.endpoint;
 
 import com.roman.podcastplayer.entity.Channel;
+import com.roman.podcastplayer.entity.Podcast;
 import com.roman.podcastplayer.manage.ChannelManager;
+import com.roman.podcastplayer.manage.PodcastManager;
 import com.roman.podcastplayer.rest.dto.PodcastDto;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,11 +18,14 @@ import java.util.stream.Collectors;
 public class PodcastEndpoint {
 
     @Inject
-    private ChannelManager manager;
+    private ChannelManager channelManager;
+
+    @Inject
+    private PodcastManager podcastManager;
 
     @GET
     public Response getPodcasts(@PathParam("channelId") Integer channelId) {
-        Channel channel = manager.findChannelById(channelId);
+        Channel channel = channelManager.findChannelById(channelId);
 
         List<PodcastDto> podcastDtos = channel.getPodcasts().stream()
                 .map(PodcastDto::new)
@@ -32,5 +35,25 @@ public class PodcastEndpoint {
 
     }
 
+    @GET
+    @Path("/{podcastId}")
+    public Response getPodcast(@PathParam("podcastId") Integer podcastId, String f) {
+        Podcast podcast = podcastManager.findPodcastById(podcastId);
+
+        if (podcast == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        PodcastDto podcastDto = new PodcastDto(podcast);
+
+        return Response.ok(podcastDto).build();
+    }
+
+    @POST
+    @Path("/{podcastId}")
+    public Response setStarred(@PathParam("podcastId") Integer podcastId, @QueryParam("s") boolean starred) {
+        podcastManager.setStarred(podcastId, starred);
+        return Response.ok().build();
+    }
 
 }
